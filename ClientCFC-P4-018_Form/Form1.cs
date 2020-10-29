967d39191ba111205bea45706e9558a199fba60e
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2020-10-29 12:21:30 +0700

    Add project files.

---
+++ ClientCFC-P4-018_Form/Form1.cs
@@ -0,0 +1,37 @@
+﻿using ServiceMtk_P1_20180140018;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientCFC_P4_018_Form
+{
+    public partial class Form1 : Form
+    {
+        BasicHttpBinding bind = new BasicHttpBinding();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+
+            int a, b, c;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a + b;
+            textBox9.Text = c.ToString();
+
+        }
+    }
+}
---
+++ ClientCFC-P4-018_Form/IMatematika.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20180140018
+{
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+
+        int Tambah(int a, int b); 
+        [OperationContract]
+        int Kurang(int a, int b);
+        [OperationContract]
+        int Kali(int a, int b);
+        [OperationContract]
+        int Bagi(int a, int b);
+        [OperationContract]
+        Koordinat TKoordinat(Koordinat a, Koordinat b); 
+
+    }
+
+    public class Koordinat
+    {
+
+    }
+
+
+}
